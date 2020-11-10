fbd4d8ab26481c5e67d934ffba0cb1f32cbee826
jhojanpa94@gmail.com <jhojanpa94@gmail.com>
2020-11-10 03:12:30 -0400

    se agrego principal

---
+++ DMC/ejemplo01/principal.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	cout<<"nombre: Jhojan Enrique Penaranda Orellana\n"
+	       "carrera: Ingeneira de sistemas\n"
+		   "ci: 10386356\n"
+	       "cu: 35-4571\n"<< endl;
+    system("pause");
+	return 0;
+}
